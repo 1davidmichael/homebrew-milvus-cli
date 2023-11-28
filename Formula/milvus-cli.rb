4e4bbeb23e4970da1a6b1f81a2d31de17db73231
David Michael <david.michael@dotdashmdp.com>
2023-11-28 08:34:42 -0600

    Initial commit

---
+++ Formula/milvus-cli.rb
@@ -0,0 +1,26 @@
+class MilvusCLI < Formula
+    include Language::Python::Virtualenv
+
+    desc "Milvus CLI"
+    homepage "https://milvus.io/docs/cli_overview.md"
+
+    url "https://files.pythonhosted.org/packages/db/d3/141e2bb58af96229fd89b9da8855eadc5cc50e54c091418b9009c2291d54/milvus_cli-0.4.0.tar.gz"
+    sha256 "80c838a7783b38aeacf0957d88fed28a55a89d47202fd9bb254956290311049a"
+    license "Apache-2.0"
+
+    depends_on "python-setuptools" => :build
+    depends_on "python@3.12"
+
+    def python3
+        which("python3.12")
+    end
+
+    def install
+        virtualenv_install_with_resources
+    end
+
+    test do
+        output = shell_output("#{bin}/milvus-cli --version")
+        assert_match "Milvus_CLI v0.4.0", output
+    end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew Milvus CLI
+
+This repository contains the homebrew formula for the Milvus CLI.
+
+## Installation
+
+```bash
+brew tap 1davidmichael/milvus-cli
+brew install milvus-cli
+```
